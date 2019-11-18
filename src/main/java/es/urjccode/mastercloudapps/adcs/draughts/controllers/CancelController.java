c0d0d4ffaa6d019845ee1e7825301346ff06ac01
Carlos <ccorcobado@gmail.com>
2019-11-18 12:57:43 +0100

    Caso de uso 'Cancelar partida' - Controller
    Hacemos funcionar la prueba

--- src/main/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/CancelController.java
+++ src/main/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/CancelController.java
@@ -1,8 +1,18 @@
 package es.urjccode.mastercloudapps.adcs.draughts.controllers;
 
+import es.urjccode.mastercloudapps.adcs.draughts.models.State;
+
 class CancelController {
 
-    void cancel(boolean b) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }    
+    private State state;
+
+    public CancelController(State state) {
+        this.state = state;
+    }
+
+    void cancel(boolean cancel) {
+        if (cancel) {
+            this.state.next();
+        }
+    }
 }
